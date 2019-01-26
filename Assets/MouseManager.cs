429e5e1488f8dc5b5c172640f782edbe31551a69
Tatan Poker <christian@eilers.cl>
2019-01-26 16:40:49 -0300

    Diego fix

---
+++ Assets/MouseManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour {
+	public float scrollSpeed = 0.2f;
+
+	public float tolerance = 2f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var mouseX = Input.mousePosition.x;
+		if (mouseX < tolerance) {
+			Camera.main.transform.position += Vector3.left * scrollSpeed;
+		} else if (mouseX > Screen.width - tolerance) {
+			Camera.main.transform.position -= Vector3.left * scrollSpeed;
+		}
+	}
+}
