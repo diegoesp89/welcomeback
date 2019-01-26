ec3671abbbd7aaed13f34b4f0166bf34d25df011
Tatan Poker <christian@eilers.cl>
2019-01-25 22:38:50 -0300

    First Commit

---
+++ Assets/ImageCycler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageCycler : MonoBehaviour {
+	public Sprite[] images;
+
+	private int currentImage;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnMouseOver() {
+		if(Input.GetMouseButtonDown(0)) {
+			CycleImage();
+		}
+	}
+
+	private void CycleImage() {
+		currentImage++;
+		if (currentImage >= images.Length) {
+			currentImage = 0;
+		}
+
+		GetComponent<SpriteRenderer>().sprite = images[currentImage];
+	}
+}
---
+++ Assets/SoundCycler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundCycler : MonoBehaviour {
+	public AudioClip[] sounds;
+
+	private int currentSound;
+	
+
+	public void OnMouseOver() {
+		if(Input.GetMouseButtonDown(0)) {
+			CycleSound();
+		}
+	}
+
+	private void CycleSound() {
+		currentSound++;
+		if (currentSound >= sounds.Length) {
+			currentSound = 0;
+		}
+
+		GetComponent<AudioSource>().clip = sounds[currentSound];
+	}
+}
