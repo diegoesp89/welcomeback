7bbd1982d7ca3ce13b78a602b09c7a23bc4b8124
Tatan Poker <christian@eilers.cl>
2019-01-25 23:42:54 -0300

    Working on clue system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,10 @@
 
-using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
 
 public class GameManager : UnityEngine.MonoBehaviour {
     private int[] combination;
+    private String[] clues;
     
     
     public void StartGame() {
@@ -14,5 +16,27 @@ public class GameManager : UnityEngine.MonoBehaviour {
             combination[i] = Random.Range(0, 2); //0, 1 or 2.
         }
         //GENERATES AN ARRAY OF N ELEMENTS, EACH INDEX IS AN ID OF AN OBJECT. THE NUMBERS INSIDE CORRESPOND TO THE VARIATIONS.
+    }
+
+    public void GenerateClues(int clueCount) {
+        /*
+         * La idea sería conseguir clueCount cantidad de GetClueTemplate para poder armar las pistas iniciales y poder
+         * darselas a nuestro usuario.
+         */
+    }
+
+    /**
+     * Gets a number and returns the corresponding template.
+     */
+    public String GetClueTemplate(int number) {
+        /*
+         * Esta es una idea de como generar los tipos de pistas.
+         */
+        switch (number) {
+            case 0:
+                return "The %0% likes the same TV Volume as the %1%";
+            default:
+                return "An error has ocurred. Rip Game.";
+        }
     }
 }
