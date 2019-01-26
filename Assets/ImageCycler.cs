a7fe919950610bf9bb4c0d4a7c8f4761fc189543
Tatan Poker <christian@eilers.cl>
2019-01-25 23:15:18 -0300

    First Commit

--- Assets/ImageCycler.cs
+++ Assets/ImageCycler.cs
@@ -2,22 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
 public class ImageCycler : MonoBehaviour {
 	public Sprite[] images;
 
 	private int currentImage;
 	
 	
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
 	public void OnMouseOver() {
 		if(Input.GetMouseButtonDown(0)) {
 			CycleImage();
