9544775b44f023e5b9ca6188077a441a5fa409ec
Nicholas Mc-Donnell <N9199@users.noreply.github.com>
2019-01-26 10:25:07 -0300

    Add most of CheckValidClues

--- Assets/Scripts/ClueHinter.cs
+++ Assets/Scripts/ClueHinter.cs
@@ -3,21 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ClueHinter : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+public class ClueHinter : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
     private string[] clues;
-    public string[] GenerateClues(int clueCountPositive, int clueCountNegative, int clueCountMixed) {
+    public string[] GenerateClues(int clueCountPositive, int clueCountNegative, int clueCountMixed)
+    {
 
         int count = 0;
-        for(int i = 0; i < clueCountPositive; i++){
+        for (int i = 0; i < clueCountPositive; i++)
+        {
             clues[i] = GetClueTemplate(0);
             count++;
         }
-        for(int i = 0; i < clueCountNegative; i++){
-            clues[i+count] = GetClueTemplate(1);
+        for (int i = 0; i < clueCountNegative; i++)
+        {
+            clues[i + count] = GetClueTemplate(1);
             count++;
         }
-        for(int i = 0; i < clueCountMixed; i++){
-            clues[i+count] = GetClueTemplate(2);
+        for (int i = 0; i < clueCountMixed; i++)
+        {
+            clues[i + count] = GetClueTemplate(2);
             count++;
         }
 
@@ -34,7 +39,7 @@ public class ClueHinter : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     public string[] reshuffle(string[] clues)
     {
         // Knuth shuffle algorithm :: courtesy of Wikipedia :)
-        for (int i = 0; i < clues.Length; i++ )
+        for (int i = 0; i < clues.Length; i++)
         {
             string tmp = clues[i];
             int rand = Random.Range(i, clues.Length);
@@ -45,14 +50,90 @@ public class ClueHinter : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
         return clues;
     }
 
+    /** 
+     * Gets Clues in a non-String Format and checks if it's enough information to solve the puzzle
+     */
+    private bool CheckValidClues()
+    {
+        int Objects = 8, Variants = 3;
+
+        int[,] SolArray = new int[Objects, Variants]; //Objects, Variants
+        //Fill SolArray
+        for (size_t i = 0; i < Objects; i++) for (size_t j = 0; j < Variants; j++)
+                SolArray[i][j] = -1;
+
+        //First Pass /Preprocessing
+        //Heavily Depends on Format of Clues
+
+        //Processing
+        bool change = true;
+        int count = 0;
+        while (change)
+        {
+            count++;
+            change = false;
+            //Check if there's enough info for each Object
+            for (size_t i = 0; i < Objects; i++)
+            {
+                //Count Negatives (0) and Positives (1)
+                int neg = 0;
+                int pos = 0;
+                for (size_t j = 0; j < Variants; j++)
+                {
+                    neg += (SolArray[i][j] == 0);
+                    pos += (SolArray[i][j] == 1);
+                }
+
+                if (pos == 1)
+                    continue;
+                if (pos > 1)
+                {
+                    //Something went wrong
+                }
+                if (neg == Variants)
+                {
+                    //Something went wrong
+                }
+                if (neg == Variants - 1)
+                {
+                    change = true;
+                    for (size_t j = 0; j < Variants; j++) if (SolArray[i][j] != 0)
+                        {
+                            int o, v;
+                            //Get o and v (depends on preprocessing)
+
+                            if (SolArray[o][v] == 1)
+                            {
+                                //Something went wrong
+                            }
+                            SolArray[i][j] = 1; //This has to be true (all others variants are false)
+                            SolArray[o][v] = 0; //This can't be true
+                            break;
+                        }
+                }
+            }
+        }
+        int solved = 0;
+        for (size_t i = 0; i < Objects; i++) for (size_t j = 0; j < Variants; j++) solved += (SolArray[i][j] == 1);
+
+        if (solved < Objects)
+        {
+            //Unsolvable
+            return false;
+        }
+        return true;
+    }
+
     /**
      * Gets a number and returns the corresponding template.
      */
-    public string GetClueTemplate(int number) {
+    public string GetClueTemplate(int number)
+    {
         /*
          * Esta es una idea de como generar los tipos de pistas.
          */
-        switch (number) {
+        switch (number)
+        {
             case 0:
                 return "I like the %o% like %s%";
             case 1:
@@ -64,11 +145,12 @@ public class ClueHinter : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
         }
     }
 
-    public void ShowClues() {
+    public void ShowClues()
+    {
         //TODO SHOW BOX FOR HINTS
     }
-    
-    
+
+
     public void OnPointerEnter(PointerEventData eventData)
     {
         Debug.Log("woo");
@@ -80,7 +162,8 @@ public class ClueHinter : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
         Debug.Log("woo");
         this.GetComponent<RectTransform>().localScale = new Vector3(1f, 1f);
     }
-    private void OnMouseExit() {
+    private void OnMouseExit()
+    {
         this.transform.localScale = new Vector3(1f, 1f);
     }
 }
